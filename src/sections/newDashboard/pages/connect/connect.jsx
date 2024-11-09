04a0f670d27355ee139c0a20e0e29383a5968973
Aman Kumar <trulyaman25@gmail.com>
2024-11-09 14:24:42 +0530

    Resolved Minor Bugs

--- src/sections/newDashboard/pages/connect/connect.jsx
+++ src/sections/newDashboard/pages/connect/connect.jsx
@@ -2,7 +2,7 @@ function Connect() {
     return (
         <>
             <main className="box-border w-screen h-screen bg-slate-200 rounded-[50px] p-20">
-                <h1 className="font-albulaHeavy text-4xl text-slate-800">Connect</h1>
+                <h1 className="font-albulaHeavy text-4xl text-white">Connect</h1>
             </main>
         </>
     )
